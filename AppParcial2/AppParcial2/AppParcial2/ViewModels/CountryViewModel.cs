36cd848ad181044e1d148e9da57812787b55af20
Jhon Alexander Romero Arancibia <a2017210251@estudiantes.upsa.edu.bo>
2020-06-09 20:27:37 -0400

    Prueba Parcial 2 (Country)

---
+++ AppParcial2/AppParcial2/AppParcial2/ViewModels/CountryViewModel.cs
@@ -0,0 +1,72 @@
+﻿using AppParcial2.Models;
+using AppParcial2.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppParcial2.ViewModels
+{ 
+    public class CountryViewModel:BaseViewModel
+    {
+
+        #region Services
+
+        private ApiService apiService;
+
+        #endregion
+
+
+        #region Attributes
+        private string name;
+        private string alpha2Code;
+        private string capital;
+
+        #endregion
+        #region Properties
+
+        public string Name 
+        {
+                get { return this.name; }
+                set { SetValue(ref this.name, value); }
+            
+        }
+        public string Capital
+        {
+            get { return this.capital; }
+            set { SetValue(ref this.capital, value); }
+        }
+        public string Alpha2Code
+        {
+            get { return this.alpha2Code; }
+            set { SetValue(ref this.alpha2Code, value); }
+        }
+
+        #endregion
+
+        #region Constructor
+        public CountryViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadCountry();
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadCountry()
+        {
+            var country = await this.apiService.Get<Country>(
+                "https://restcountries.eu/rest/v2/",
+                "name/",
+                "bolivia"
+                );
+            this.Name = country[0].Name;
+            this.Capital = country[0].Capital;
+            this.Alpha2Code = country[0].Alpha2Code;
+        }
+
+
+        #endregion
+
+
+    }
+}
